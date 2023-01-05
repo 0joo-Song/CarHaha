1a805eaf46376a20d37e595621bb47366192b9e4
0joo <syoung7024@gmail.com>
2023-01-05 23:38:00 +0900

    setting

---
+++ carHaha_web/src/test/java/com/chh/CarHahaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarHahaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
